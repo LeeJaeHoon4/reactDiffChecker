75a183819f1a92f5fe77eed61b16abd494cff77c
LeeJaeHoon95 <velo999@creamhouse.net>
2025-04-04 11:41:15 +0900

    env에서 운영 및 dev 포트 관리

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+const AXIOS_BASE_URL = process.env.REACT_APP_AXIOS_BASE_URL || 'http://localhost';
+const AXIOS_PORT = process.env.REACT_APP_AXIOS_API_PORT || '8080';
+const API_URL = `${AXIOS_BASE_URL}:${AXIOS_PORT}`;
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: API_URL,
     timeout: 55000,
     withCredentials: true,            // 세션/쿠키 사용 시 반드시 필요
     headers: {
@@ -10,3 +13,4 @@ const instance = axios.create({
 });
 
 export default instance;
+
